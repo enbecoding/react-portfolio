945259f4262b323449a878fe5c7c63219597993f
Brandon <brandon.nhoang@gmail.com>
2022-10-18 02:18:46 -0500

    added resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import styles from "./Header.module.css"
 
+
 const Header = () => {
 
   return (
     <>
     <header>
-      <h1 className={styles.title}></h1>
+        <a 
+          href="https://docs.google.com/document/d/1NAVauXftuvXuhBgkjuLIgZ6KqKdRp-OFTONf3Cjk6fk/edit"
+          rel="noreferrer"
+          target="_blank"
+          className={styles.nav_btn}
+        >
+          Resume
+        </a>
     </header>
     </>      
   )
