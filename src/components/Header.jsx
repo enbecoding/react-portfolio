9cff62c1dc97369356d25da9cbb66bcb31aca98f
Brandon <brandon.nhoang@gmail.com>
2022-10-18 02:08:43 -0500

    current build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import {Link} from "react-router-dom"
 import styles from "./Header.module.css"
 
 const Header = () => {
+
   return (
     <>
     <header>
-      <h2></h2>
-          <button className={styles.nav_btn}>Contact Me
-          </button>
+      <h1 className={styles.title}></h1>
     </header>
     </>      
   )
